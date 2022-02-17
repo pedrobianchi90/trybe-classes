ac2ae33278833247e9380e3daa94ff01dcf6f81a
Pedro Bianchi <pedroedm@gmail.com>
2022-02-17 08:25:20 -0300

    Aula 10.1

---
+++ front-end/Bloco-10-Introducao-ao-React/10.1-Hello-World-no-React/projeto-teste/src/Component.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Component extends React.Component {
+    render() {
+        return <div>
+            <h1>Pedro Bianchi</h1>
+            <p>Hello World!</p>
+        </div>
+    }
+}
+
+export default Component;
